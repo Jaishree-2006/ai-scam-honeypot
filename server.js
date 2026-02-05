f81c9454b3697d9ae5a1a7f9898ca8215b61187d
Jaishree M <jaishreem.it2024@citchennai.net>
2026-02-05 21:28:35 +0530

    add vercel support

--- server.js
+++ server.js
@@ -216,6 +216,10 @@ app.post("/detect-scam", async (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+if (!process.env.VERCEL) {
+  app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+  });
+}
+
+export default app;
